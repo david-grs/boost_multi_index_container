7d1039c3d524f93195f79efd3f8bf86a70e7d062
David G <david.a.grs@gmail.com>
2016-10-14 20:51:25 +0200

    adding employee example

---
+++ employee.cc
@@ -0,0 +1,15 @@
+#include <string>
+#include <map>
+
+struct employee
+{
+    std::string first_name;
+    std::string last_name;
+    int age;
+    double salary;
+};
+
+int main()
+{
+    return 0;
+}

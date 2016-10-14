a6f3ebd1b065e643a52b745f82b018644d6afacd
David G <david.a.grs@gmail.com>
2016-10-14 21:22:02 +0200

    employee adding composed key example

--- employee.cc
+++ employee.cc
@@ -5,6 +5,7 @@
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/hashed_index.hpp>
 #include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/composite_key.hpp>
 
 #include <iostream>
 
@@ -20,7 +21,7 @@ struct employee
 
 struct by_name{};
 
-int main()
+void simple_index()
 {
     boost::multi_index_container<
       employee,
@@ -37,6 +38,36 @@ int main()
 
     for (auto&& e : v)
       std::cout << e.first_name << std::endl;
+}
+
+void composed_index()
+{
+    boost::multi_index_container<
+      employee,
+      indexed_by<
+        hashed_unique<
+          tag<by_name>,
+          composite_key<
+            employee,
+            BOOST_MULTI_INDEX_MEMBER(employee, std::string, first_name),
+            BOOST_MULTI_INDEX_MEMBER(employee, std::string, last_name)
+          >
+        >
+      >
+    > employees;
+
+    auto&& v = employees.get<by_name>();
+    v.insert({"john", "doe", 21, 2000.0});
+
+    auto it = v.find(boost::make_tuple("john", "doe"));
+    std::cout << it->first_name << std::endl;
+}
+
+
+int main()
+{
+    simple_index();
+    composed_index();
 
     return 0;
 }

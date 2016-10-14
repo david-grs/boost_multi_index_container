f76ac72dbe116f7028787c183294292b02f2335e
David G <david.a.grs@gmail.com>
2016-10-14 21:11:30 +0200

    employee: adding basic usage on single hashed index

--- employee.cc
+++ employee.cc
@@ -1,15 +1,42 @@
 #include <string>
 #include <map>
 
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+
+#include <iostream>
+
+using namespace boost::multi_index;
+
 struct employee
 {
-    std::string first_name;
-    std::string last_name;
-    int age;
-    double salary;
+  std::string first_name;
+  std::string last_name;
+  int age;
+  double salary;
 };
+
+struct by_name{};
 
 int main()
 {
+    boost::multi_index_container<
+      employee,
+      indexed_by<
+        hashed_unique<
+            tag<by_name>,
+            BOOST_MULTI_INDEX_MEMBER(employee, std::string, first_name)
+        >
+      >
+    > employees;
+
+    auto&& v = employees.get<by_name>();
+    v.insert({"john", "doe", 21, 2000.0});
+
+    for (auto&& e : v)
+      std::cout << e.first_name << std::endl;
+
     return 0;
 }

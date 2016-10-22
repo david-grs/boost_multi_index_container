899df1044158921566cc0299bebee96541f0b0b2
David G <david.a.grs@gmail.com>
2016-10-22 23:23:06 +0200

    adding tracker<T>

--- memory.cc
+++ memory.cc
@@ -1,27 +1,51 @@
-#include "counter.h"
-
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/hashed_index.hpp>
 #include <boost/multi_index/mem_fun.hpp>
 #include <boost/multi_index/composite_key.hpp>
 
-#include <string>
-#include <map>
-#include <experimental/string_view>
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace boost::multi_index;
 
-struct key
+template <typename T>
+struct tracker
+{
+    tracker()
+    {
+        s_objects.insert(this);
+    }
+    tracker(const tracker&)
+    {
+        s_objects.insert(this);
+    }
+    tracker(tracker&&)
+    {
+        s_objects.insert(this);
+    }
+
+    ~tracker()
+    {
+        int deleted = s_objects.erase(this);
+        assert(deleted == 1);
+    }
+
+private:
+    static std::unordered_set<void*> s_objects;
+};
+
+template <typename T>
+std::unordered_set<void*> tracker<T>::s_objects;
+
+struct key : tracker<key>
 {
     explicit key(int k) : _k(k) {}
     bool operator==(const key& rhs) const { return _k == rhs._k; }
     int _k;
 };
 
-struct value
+struct value : tracker<value>
 {
     explicit value(int k) : _k(k) {}
     bool operator==(const value& rhs) const { return _k == rhs._k; }
